6e4607aaa775b09ec441225336103abc978e183b
Greg Szabo <greg@philosobear.com>
2020-07-11 07:04:27 -0400

    fmt fix

--- ibc_proto/src/lib.rs
+++ ibc_proto/src/lib.rs
@@ -2,27 +2,27 @@
 //  This module setup is necessary because the generated code contains "super::" calls for dependencies.
 
 mod cosmos {
-    include!(concat!(env!("OUT_DIR"),"/cosmos.rs"));
+    include!(concat!(env!("OUT_DIR"), "/cosmos.rs"));
     pub mod query {
-        include!(concat!(env!("OUT_DIR"),"/cosmos.query.rs"));
+        include!(concat!(env!("OUT_DIR"), "/cosmos.query.rs"));
     }
 }
 
 mod ibc {
     pub mod channel {
-        include!(concat!(env!("OUT_DIR"),"/ibc.channel.rs"));
+        include!(concat!(env!("OUT_DIR"), "/ibc.channel.rs"));
     }
     pub mod commitment {
-        include!(concat!(env!("OUT_DIR"),"/ibc.commitment.rs"));
+        include!(concat!(env!("OUT_DIR"), "/ibc.commitment.rs"));
     }
     pub mod connection {
-        include!(concat!(env!("OUT_DIR"),"/ibc.connection.rs"));
+        include!(concat!(env!("OUT_DIR"), "/ibc.connection.rs"));
     }
     pub mod localhost {
-        include!(concat!(env!("OUT_DIR"),"/ibc.localhost.rs"));
+        include!(concat!(env!("OUT_DIR"), "/ibc.localhost.rs"));
     }
     pub mod transfer {
-        include!(concat!(env!("OUT_DIR"),"/ibc.transfer.rs"));
+        include!(concat!(env!("OUT_DIR"), "/ibc.transfer.rs"));
     }
 }
 
@@ -30,17 +30,17 @@ mod tendermint {
     pub mod abci {
         #[allow(clippy::large_enum_variant)]
         pub mod types {
-            include!(concat!(env!("OUT_DIR"),"/tendermint.abci.types.rs"));
+            include!(concat!(env!("OUT_DIR"), "/tendermint.abci.types.rs"));
         }
     }
     pub mod crypto {
         pub mod merkle {
-            include!(concat!(env!("OUT_DIR"),"/tendermint.crypto.merkle.rs"));
+            include!(concat!(env!("OUT_DIR"), "/tendermint.crypto.merkle.rs"));
         }
     }
     pub mod libs {
         pub mod kv {
-            include!(concat!(env!("OUT_DIR"),"/tendermint.libs.kv.rs"));
+            include!(concat!(env!("OUT_DIR"), "/tendermint.libs.kv.rs"));
         }
     }
 }
