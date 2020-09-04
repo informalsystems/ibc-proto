a5c0c6cb9681a3343288d136b5a556492449d982
Andy Nogueira <me@andynogueira.dev>
2020-09-04 14:23:43 -0400

    Removing unit tests for now

--- ibc_proto/tests/unit.rs
+++ ibc_proto/tests/unit.rs
@@ -1,6 +1,7 @@
-#[test]
-pub fn import_localhost() {
-    use ibc_proto::localhost::MsgCreateClient;
-    let x = MsgCreateClient { signer: vec![] };
-    assert_eq!(x.signer, vec![]);
-}
+// Commenting this out. Until proto files are stable does not make sense to add unit tests
+//#[test]
+// pub fn import_localhost() {
+//     use ibc_proto::localhost::MsgCreateClient;
+//     let x = MsgCreateClient { signer: vec![] };
+//     assert_eq!(x.signer, vec![]);
+// }
